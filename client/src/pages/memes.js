7a50b72d1fd3d09f39bf0d89091a048da3aa836b
Allison Wei <weiallison1999@gmail.com>
2021-04-10 23:39:14 -0400

    DANK MEMESSS

--- client/src/pages/memes.js
+++ client/src/pages/memes.js
@@ -1,9 +1,43 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const memeLinks = [
+  {
+    url:
+      "https://64.media.tumblr.com/668f8fa044b09642396ee8be9846b449/tumblr_olspqaVPMy1u9ru6ro1_500.png",
+    alt: "gg bruh",
+  },
+  {
+    url:
+      "https://64.media.tumblr.com/668f8fa044b09642396ee8be9846b449/tumblr_olspqaVPMy1u9ru6ro1_500.png",
+    alt: "gg bruh",
+  },
+  {
+    url:
+      "https://64.media.tumblr.com/668f8fa044b09642396ee8be9846b449/tumblr_olspqaVPMy1u9ru6ro1_500.png",
+    alt: "gg bruh",
+  },
+  {
+    url:
+      "https://qph.fs.quoracdn.net/main-qimg-6d32bddfb0b0e98a46bb87d8cfafa8e1.webp",
+    alt: "gg bruh",
+  },
+];
 
 const Memes = () => {
   return (
     <div>
-      <h1>Memes</h1>
+      <Typography variant="h1" align="center">
+        dank memes
+      </Typography>
+      <Grid spacing={5} container direction="column" alignItems="center">
+        {memeLinks.map(({ url, alt }) => (
+          <Grid item>
+            <img src={url} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
