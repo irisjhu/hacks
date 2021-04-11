678656d89d3e9f63a192ffab1e632dd0d9584716
Iris Hu <ihu009@gmail.com>
2021-04-10 23:46:27 -0400

    basic about us page with name&shape on hover, with 12 iris's

---
+++ client/src/components/MemberCard/memberCard.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Grid, Card, CardMedia, Fade } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const MemberCard = (props) => {
+  const classes = useStyles();
+  const [showInfo, setShowInfo] = useState(false);
+
+  const onMouseHover = () => {
+    if (!showInfo) {
+      setShowInfo(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (showInfo) {
+      setShowInfo(false);
+    }
+  };
+
+  return (
+    <Grid item xs={12} sm={6} md={3}>
+      <Card className={classes.card}>
+        <CardMedia
+          component="img"
+          image={props.member}
+          onMouseOver={onMouseHover}
+        />
+        <Fade in={showInfo} timeout={500}>
+          <div onMouseLeave={onMouseLeave}>
+            <div className={classes.overlayPic}>
+              <CardMedia component="img" image={props.shape} />
+            </div>
+            <div className={classes.overlayText}>Iris Hu</div>
+          </div>
+        </Fade>
+      </Card>
+    </Grid>
+  );
+};
+
+export default MemberCard;
