c758972ea14a7e30e9883e7ee317923fd4a6e569
Iris Hu <ihu009@gmail.com>
2021-04-11 12:08:08 -0400

    fix background image offset

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -18,50 +18,52 @@ const Home = () => {
     // >
     // </Box>
     <div
-    style = {{
-      position: "fixed",
-top: "50",
-width: "100%",
-height: "100%",
-backgroundImage: `url(${textbackground})`,
-backgroundSize: "cover",
-    }}>
-    <div
-    style = {{
-      position: "fixed",
-top: "50",
-width: "100%",
-height: "100%",
-backgroundImage: `url(${background})`,
-backgroundSize: "cover"
-    }}>
-    <div
       style={{
-        position: "absolute",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
+        position: "fixed",
+        top: "50px",
+        width: "100%",
+        height: "100%",
+        backgroundImage: `url(${textbackground})`,
+        backgroundSize: "cover",
       }}
     >
-      {/* https://css-tricks.com/almanac/properties/s/stroke-linecap/ */}
-      <Typography
-        className={classes.heading}
-        variant="h1"
-        align="center"
-        size="big"
+      <div
+        style={{
+          position: "fixed",
+          top: "50px",
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+        }}
       >
-        hacks
-      </Typography>
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          {/* https://css-tricks.com/almanac/properties/s/stroke-linecap/ */}
+          <Typography
+            className={classes.heading}
+            variant="h1"
+            align="center"
+            size="big"
+          >
+            hacks
+          </Typography>
 
-      <Typography
-        variant="h5"
-        align="center"
-        className={classes.alignItemsAndJustifyContent}
-      >
-        homies that are cool kids
-      </Typography>
-    </div>
-    </div>
+          <Typography
+            variant="h5"
+            align="center"
+            className={classes.alignItemsAndJustifyContent}
+          >
+            homies that are cool kids
+          </Typography>
+        </div>
+      </div>
     </div>
   );
 };
