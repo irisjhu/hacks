68164f26584daa1b52441e2310e91fb23926d9d8
Iris Hu <ihu009@gmail.com>
2021-05-02 16:10:11 -0400

    remove example files

--- server/controllers/example.js
+++
@@ -1,3 +0,0 @@
-export const getExample = (req, res) => {
-  res.send("This works!");
-};
--- server/routes/example.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getExample } from "../controllers/example.js";
-
-const router = express.Router();
-
-router.get("/", getExample);
-
-export default router;
