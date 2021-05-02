d301d3640e540478a1130f74c530d5b259ca20a6
Iris Hu <ihu009@gmail.com>
2021-05-02 00:50:25 -0400

    add dialog modal popup to host multi-step form

---
+++ client/src/components/FormDialog/FormDialog.js
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import CloseIcon from "@material-ui/icons/Close";
+
+import useStyles from "./styles";
+
+const FormDialog = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        {props.buttonText}
+      </Button>
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+        <DialogTitle disableTypography>
+          <Typography variant="h6">{props.title}</Typography>
+          <IconButton className={classes.closeButton} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>{props.content}</DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default FormDialog;
