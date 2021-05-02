f984b0010a60f01e2c0f17c674f692b6f3196900
Iris Hu <ihu009@gmail.com>
2021-05-02 00:50:52 -0400

    add button to trigger dialog modal on gallery page

--- client/src/pages/gallery/gallery.js
+++ client/src/pages/gallery/gallery.js
@@ -1,10 +1,30 @@
+import { Box, Container, Typography } from "@material-ui/core";
 import React from "react";
 
+import FormDialog from "../../components/FormDialog/FormDialog";
+import UserForm from "../../components/MultiStepForm/UserForm";
+
+import useStyles from "./styles";
+
 const Gallery = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Gallery</h1>
-    </div>
+    <Container>
+      <Typography variant="h1" align="center">
+        <Box paddingTop={10} paddingBottom={5} letterSpacing={6}>
+          gallery
+        </Box>
+      </Typography>
+      <Container className={classes.centerItems}>
+        <FormDialog
+          buttonText="Upload Image"
+          title="Upload Image"
+          content={<UserForm />}
+        />
+      </Container>
+      {/* TODO: fetch images from database and display in grid */}
+    </Container>
   );
 };
 
